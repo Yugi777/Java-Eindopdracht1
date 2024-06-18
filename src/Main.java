d1349f7288449f4525d43c1ab780c1096e9ecd5b
Yugi777 <147806229+Yugi777@users.noreply.github.com>
2024-06-18 09:39:46 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import javax.imageio.metadata.IIOMetadataFormatImpl;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("*");
+        }
+
+        System.out.println(" ");
+        System.out.println("opdracht 2");
+
+        for (int k = 0; k <6 ; k++)
+        {
+            for (int j = 0; j <k ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+        System.out.println(" ");
+        System.out.println("opdracht 3");
+        for (int w = 0; w <6 ; w++)
+        {
+            for (int v = 6; v >w ; v--) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+
+
+            }
+
+        }
+
+
+
+
+
+
